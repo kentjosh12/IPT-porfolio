e3b77deeacacdbde70870a6a321b2f036708b502
kentjosh12 <kentjosh.gutierrez@hcdc.edu.ph>
2024-10-17 22:19:18 +0800

    Initial commit with all project files

---
+++ about-me/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AboutMe from './components/AboutMe';
+import  './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Portfolio</h1>
+      </header>
+      <AboutMe />
+    </div>
+  );
+};
+
+export default App;
